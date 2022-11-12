2f69d273c79417ea661fba086f88a992fa35dbec
shograti <echecsleo@gmail.com>
2022-11-12 16:07:21 +0100

    Added display of additional informations to photographer's portraits

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,31 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement('article');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    const priceText = document.createElement('p');
+
+    img.setAttribute('src', picture);
+    h2.textContent = name;
+    h3.textContent = `${city}, ${country}`;
+    p.textContent = tagline;
+    priceText.textContent = `${price}€/jour`;
+
+    priceText.classList.add('price-text')
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(p);
+    article.appendChild(priceText);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
