7a8a388b28c4a708ef3d9cb9a14771238168b31d
shograti <echecsleo@gmail.com>
2022-11-12 15:53:44 +0100

    Fetched photographers and displayed it on homepage

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,8 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    const photographers = await fetch('../../data/photographers.json');
+    return photographers.json();
+  }
+  
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -36,7 +15,6 @@
     };
 
     async function init() {
-        // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
