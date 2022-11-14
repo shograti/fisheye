957a336294db95aef5bd66d9ae2fe6e5b7b561b6
shograti <echecsleo@gmail.com>
2022-11-14 15:43:05 +0100

    Added contact functionality

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,42 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById('contact_modal');
+  modal.style.display = 'flex';
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById('contact_modal');
+  modal.style.display = 'none';
 }
+
+function init() {
+  const inputs = document.querySelectorAll('input, textarea');
+  const submitButton = document.querySelector('.submit_button');
+  const formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  };
+
+  function clearModal() {
+    inputs.forEach((input) => {
+      input.value = '';
+    });
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+      const key = e.target.name;
+      formData[key] = e.target.value;
+    });
+  });
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(formData);
+    clearModal();
+    closeModal();
+  });
+}
+
+init();
